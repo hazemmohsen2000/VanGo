9ba1cd8ce05ee542ddd0e63d4d9ce74e33352398
Hazem1234321 <hazemmohsen188@gmail.com>
2024-05-12 05:15:51 +0300

    edting

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import income from "\assets\images\income-graph.png"
+
 export default function Income() {
     const transactionsData = [
         { amount: 720, date: "Jan 3, '23", id: "1" },
@@ -15,7 +15,7 @@ export default function Income() {
             <h2>$2,260</h2>
             <img
                 className="graph"
-                src={income}
+                src="/assets/images/income-graph.png"
                 alt="Income graph"
             />
             <div className="info-header">
